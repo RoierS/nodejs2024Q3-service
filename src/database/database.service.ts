8f1508584af18a6f725d0044ac6480a9770fc62f
Roman Iermoliuk <iermoliuk.roman@gmail.com>
2024-11-11 14:55:49 +0100

    feat: implement operating track resource

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -2,11 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '../user/entities/user.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { plainToClass } from 'class-transformer';
+import { Track } from 'src/track/entities/track.entity';
 
 @Injectable()
 export class DatabaseService {
   private users: User[] = [];
+  private tracks: Track[] = [];
 
+  // USERS
   getAllUsers(): User[] {
     return this.users;
   }
@@ -42,5 +45,38 @@ export class DatabaseService {
     if (index === -1) throw new NotFoundException('User not found');
 
     this.users.splice(index, 1);
+  }
+
+  // TRACKS
+  getAllTracks(): Track[] {
+    return this.tracks;
+  }
+
+  createTrack(track: Partial<Track>): Track {
+    const newTrack = new Track({ id: uuidv4(), ...track });
+    this.tracks.push(newTrack);
+
+    return newTrack;
+  }
+
+  findTrackById(id: string): Track {
+    const track = this.tracks.find((t) => t.id === id);
+    if (!track) throw new NotFoundException('Track not found');
+
+    return track;
+  }
+
+  updateTrack(id: string, updatedFields: Partial<Track>): Track {
+    const track = this.findTrackById(id);
+    Object.assign(track, updatedFields);
+
+    return track;
+  }
+
+  deleteTrack(id: string): void {
+    const index = this.tracks.findIndex((track) => track.id === id);
+    if (index === -1) throw new NotFoundException('Track not found');
+
+    this.tracks.splice(index, 1);
   }
 }
